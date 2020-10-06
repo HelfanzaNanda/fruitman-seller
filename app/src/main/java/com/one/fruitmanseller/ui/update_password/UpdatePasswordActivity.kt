765eb34448b7a5cc89f37e690af288b825c36a43
ikhwanudin23 <ikhwanudin2309@gmail.com>
2020-10-06 12:20:18 +0700

    finish

---
+++ app/src/main/java/com/one/fruitmanseller/ui/update_password/UpdatePasswordActivity.kt
@@ -0,0 +1,13 @@
+package com.one.fruitmanseller.ui.update_password
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.one.fruitmanseller.R
+
+class UpdatePasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_password)
+    }
+}
